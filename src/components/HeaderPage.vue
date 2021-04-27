4090a66ebcc8a7ca8b51f5eba1327c6383e99e5e
iquiroga <ignacioequiroga@gmail.com>
2021-04-27 01:47:20 -0300

    cambio de directiorio del logo

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="d-flex justify-content-center mb-5">
-      <img class="pokemon" src="../assets/Pokemon_logo.svg"/>
+      <img class="pokemon" src="../../public/Pokemon_logo.svg"/>
     </div>
    </header>
 </template>
